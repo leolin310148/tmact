398a3670cc7fe556d7b8233c4fa64823e76cb9ee
leolin310148 <leolin310148@gmail.com>
2026-07-05 07:53:02 +0800

    Set Allow header for file endpoint

--- internal/web/file_handlers.go
+++ internal/web/file_handlers.go
@@ -9,6 +9,7 @@ import (
 
 func (s *Server) handleFile(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
 		writeJSONError(w, http.StatusMethodNotAllowed, "method not allowed")
 		return
 	}
