2ce0748a1184e1e57d1a3b23d0b1e5164400d34b
leolin310148 <leolin310148@gmail.com>
2026-07-05 10:55:19 +0800

    Set Allow header for image 405 responses

--- internal/web/image_handlers.go
+++ internal/web/image_handlers.go
@@ -13,6 +13,7 @@ import (
 
 func (s *Server) handleImage(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
 		writeJSONError(w, http.StatusMethodNotAllowed, "method not allowed")
 		return
 	}
